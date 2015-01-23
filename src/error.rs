3aa8c4de49da5e6ddf202c8110b7ce34d5065617
Johannes Schickling <schickling.j@gmail.com>
2015-01-23 18:03:55 +0100

    fix display

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,11 @@
-#[derive(Debug)]
+use std::fmt::{Formatter,Display,Error};
+
 pub enum BeanstalkdError { ConnectionError, RequestError }
+
+impl Display for BeanstalkdError {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        Ok(())
+    }
+}
 
 pub type BeanstalkdResult<T> = Result<T, BeanstalkdError>;
