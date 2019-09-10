bb348efd230c1f05fa68b7b440d6316bd11ea698
jan <jan@stdpixel.com>
2019-09-10 23:20:23 +0200

    fixed example

--- examples/reserve_with_timeout.rs
+++ examples/reserve_with_timeout.rs
@@ -4,18 +4,10 @@ use beanstalkd::Beanstalkd;
 
 fn main() {
     let mut beanstalkd = Beanstalkd::localhost().unwrap();
-    loop {
-        match beanstalkd.reserve_with_timeout(2) {
-            Ok(Some((id, body))) => {
-                if id > 0 && &body != "TIMED_OUT" {
-                    println!("id: {} body: {}", id, body);
-                    break;
-                } else {
-                    println!("no job, do some other stuff...");
-                }
-            },
-            Ok(None) => println!("timedout"),
-            Err(err) => println!("{}", err),
-        }
+
+    match beanstalkd.reserve_with_timeout(2) {
+        Ok(Some((id, body))) => println!("id: {} body: {}", id, body),
+        Ok(None) => println!("timedout"),
+        Err(err) => println!("{}", err),
     }
 }
